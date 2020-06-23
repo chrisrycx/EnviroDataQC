13b48022ddcfd897d3ad62a341f8f4759012d678
chrisrycx <chrisrycx@gmail.com>
2020-06-23 17:42:52 -0600

    Experiment with possible range object

---
+++ experiments/ideas.py
@@ -0,0 +1,19 @@
+'''
+Test out some ideas for data checking
+'''
+
+#Use an object to store datacheck configuration?
+class qcrange:
+    '''
+    Holds the various configuration in an accessible way
+    '''
+    def __init__(self,config):
+        '''
+        Input configuration information from yaml
+        '''
+        self.type = name of measurement
+        self.good
+        self.suspicious
+    def good(self,value)
+    def flat
+    def rate
