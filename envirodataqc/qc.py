d166ac4219341b2e34b3b133e5392a368d9191f8
chrisrycx <chrisrycx@gmail.com>
2020-07-01 18:01:52 -0600

    Added method for checking range

--- envirodataqc/qc.py
+++ envirodataqc/qc.py
@@ -1,64 +1,107 @@
 '''
-Defines a class to hold QC settings and methods
+Define dataQC class
+- Objects can have multiple ranges describing good, suspicious values
+- Where suspicious and good ranges overlap, data will be classified as good
+- **rates and flat must be in units of min (for now)
+- **each method takes a pandas df with values of interest in col 1 and datetimes col 0
+- **each method returns an numpy array of flags for each value
 '''
+
+#Test data
+testgood = {
+    'range':[(0,5),(6,10)],
+    'rate':[(0,10)],
+    'flat':[(0,15)]
+}
 
 class dataqc:
     '''
-    Defines all QC settings and methods
+    dataqc class:
+    Contains QC settings and methods
     '''
-    def __init__(self,vartype):
+    def __init__(self,typename,goodvals,susvals):
         '''
         Create a new data QC object
         Input
-        --
+        - typename: type of data
+        - goodvals: a list with different good range settings
+        - susvals: a list with different suspicious range settings 
         '''
-        #Check suspicious range first
-    for susprange in airTsusp:
-        minval = qc.susprange[0]
-        maxval = qc.susprange[1]
+        #Load inputs
+        self.datatype = typename
+        
+        self.goodrange = goodvals['range']   #[(x,y),(x2,y2)...]
+        self.goodrate = goodvals['rate']   #Units/min
+        self.goodflat = goodvals['flat']   #Minutes
+        self.susprange = []
+        self.susprate = []
+        self.suspflat = []
+
+    def _check_range(self,datavals,flags,maxval,minval,rangetype):
+        '''
+        "Private" method to check values for a given range
+        Inputs
+            - datavals: numpy array of data values
+            - flags: current numpy array of flags
+            - max and min values for range
+            - rangetype = 'good' or 'suspicious' 
+        Returns
+            - np array of flags for each value
+        '''
+        ranges = {'good':0,'suspicious':1}
+        flags[(datavals > minval) and (datavals < maxval)] = ranges[rangetype] 
+
+        return flags
+        
+    
+    def check_ranges(self,data):
         data.loc[(data['vals']>minval) | (data['vals']<maxval),'flags'] = 1
 
-    #Check good range second... this will override if there was overlap
-    for goodrange in airTgood:
+        #Check good range second... this will override if there was overlap
+        for goodrange in airTgood:
         minval = qc.goodrange[0]
         maxval = qc.goodrange[1]
         data.loc[(data['vals']>minval) | (data['vals']<maxval),'flags'] = 0
-    
 
-    
+    def check_rate():
+        #Calculate absolute slope between input data points
+        vdiff = np.absolute(np.diff(data[mtype].values))
+        #Calculate time differences in minutes
+        timediff = np.diff(data.index)
+        timediff = timediff.astype(float)/(60*(10**9)) #60 x 10^9 to convert from nanosec
+        dataslopes = vdiff/timediff
 
-    #Calculate absolute slope between input data points
-    vdiff = np.absolute(np.diff(data[mtype].values))
-    #Calculate time differences in minutes
-    timediff = np.diff(data.index)
-    timediff = timediff.astype(float)/(60*(10**9)) #60 x 10^9 to convert from nanosec
-    dataslopes = vdiff/timediff
-
-    #Flag points based on jumps. Also identify flatlined sections
-    oldslopeflat = False #Keeps track of start of flatlined data
-    sindex = 0
-    counter = 0 #Keep track of index
-    for slope in dataslopes:
+        #Flag points based on jumps. Also identify flatlined sections
+        oldslopeflat = False #Keeps track of start of flatlined data
+        sindex = 0
+        counter = 0 #Keep track of index
+        for slope in dataslopes:
         
-        if (slope!=0) and (oldslopeflat):
-            #Signifies the end of a section of flat data
-            eindex = counter
-            #Check amount of time flat
-            flattime = (data.index[eindex] - data.index[sindex]).seconds/60 #Time in minutes
-            #If exceeds maximum time flag all points
-            if flattime > maxflat[mtype]:
-                data.iloc[sindex:eindex+1,1]=1
-            oldslopeflat = False
+            if (slope!=0) and (oldslopeflat):
+                #Signifies the end of a section of flat data
+                eindex = counter
+                #Check amount of time flat
+                flattime = (data.index[eindex] - data.index[sindex]).seconds/60 #Time in minutes
+                #If exceeds maximum time flag all points
+                if flattime > maxflat[mtype]:
+                    data.iloc[sindex:eindex+1,1]=1
+                oldslopeflat = False
 
-        if (slope==0) and (not oldslopeflat):
-            #Signifies the start of a flat section
-            sindex = counter
-            oldslopeflat = True
+            if (slope==0) and (not oldslopeflat):
+                #Signifies the start of a flat section
+                sindex = counter
+                oldslopeflat = True
         
-        #Slope exceeds limits
-        if abs(slope) > maxjump[mtype]:
-            #Flag points adjacent as suspicious
-            data.iloc[counter,1] = 1
-            data.iloc[counter+1,1] = 1
+            #Slope exceeds limits
+            if abs(slope) > maxjump[mtype]:
+                #Flag points adjacent as suspicious
+                data.iloc[counter,1] = 1
+                data.iloc[counter+1,1] = 1
 
-        counter = counter+1
+            counter = counter+1
+
+    def check_flat():
+            pass
+
+
+    
