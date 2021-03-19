7674f29af4d289b70ef20fa67a71aafac5653c9b
Chris Cox <chrisc@dyacon.com>
2021-03-19 15:41:59 -0600

    Updated wind algorithms and tests

--- envirodataqc/wind.py
+++ envirodataqc/wind.py
@@ -4,7 +4,7 @@ Special QC Algorithms for wind
 import numpy as np
 import pandas as pd
 
-def check_windspeed(data):
+def check_windsp_ratio(data):
     '''
     Evaluate wind speeds for internal consistency
     - Calculate ratio of max/ave values (typically for 24hr period)
@@ -32,38 +32,82 @@ def check_windspeed(data):
     return dave/maxval
     
 
-def check_winddir(data):
-    '''
-    Evaluate direction data
-    Assess flatlining in context of windspeed.
-    Flatlining associated with wind > 0 is suspicious.
-    Input
-    - dataframe: index (datetime),speedvals,dirvals
-    Return
-    - list of flags associated with each value (0 good, 1 suspicious)
-    '''
-    spvals = data.iloc[:,0].to_numpy()
-    dirvals = data.iloc[:,1].to_numpy()
+def check_windsp_withdir(wind_speed,wind_direction):
+    ''' 
+    Check that wind speed is consistent with wind direction.
+    Flag speed values suspicious if unchanging at zero wind 
+    with wind direction changing.
 
-    #Check flatlining against wind speed
-    slopeflags = []
-    for n in range(len(spvals)-1):
-        #Flag where direction change is zero but wind > 0
-        dirdiff = dirvals[n+1] - dirvals[n]
-        spsum = spvals[n+1] + spvals[n]
-        if (dirdiff == 0) and (spsum > 0):
-            slopeflags.append(1)
-        else:
-            slopeflags.append(0)
+    Inputs
+        wind_speed - numpy array of speeds
+        wind_direction - numpy array of directions
+        lengths of arrays must match
+    Output
+        flags - array of flags (0,1)
+    '''
+    #Calculate the differences between array values
+    wsdiff = np.diff(wind_speed)
+    wddiff = np.diff(wind_direction)
 
-    #Finally: assign point flags based on slope flags
-    flags = []
-    for n in range(len(slopeflags)-1):
-        flags.append(max(slopeflags[n],slopeflags[n+1]))
-    #Endpoints
-    flags.insert(0,slopeflags[0])
-    flags.append(slopeflags[-1])
+    #Identify suspicious sections: flat ws slope, changing dir
+    slope_compare = np.zeros(len(wsdiff))
+    slope_compare[(wsdiff == 0) & (wddiff != 0)] = 1 
+
+    #Loop through values assigning flags
+    flags = [0]*len(wind_speed)
+    counter = 0
+    for slope in slope_compare:
+        #Sum adjacent points to identify when points are both 0
+        valsum = wind_speed[counter] + wind_speed[counter + 1]
+        if (slope > 0) and (valsum == 0):
+            flags[counter] = 1
+            flags[counter + 1] = 1
+        
+        counter = counter + 1
 
     return flags
 
-    
+
+def check_winddir_withsp(wind_speed,wind_direction):
+    '''
+    Evaluate direction data
+    Any flatlining associated with wind speed > 0 is suspicious.
+    Inputs
+        wind_speed - numpy array of speeds
+        wind_direction - numpy array of directions
+        lengths of arrays must match
+    Output
+        flags - array of flags (0,1)
+    '''
+    #Calculate slopes between direction values
+    wddiff = np.diff(wind_direction)
+
+    #Check areas where wind direction slope is zero against wind speed
+    flags = [0]*len(wind_speed)
+    counter = 0
+    for slope in wddiff:
+        #Add adjacent wind speed values to see if both > 0
+        valsum = wind_speed[counter] + wind_speed[counter + 1]
+        if (slope == 0) & (valsum > 0):
+            flags[counter] = 1
+            flags[counter + 1] = 1
+        
+        counter = counter + 1
+
+    return flags
+
+
+#Quick test
+if __name__=='__main__':
+    '''
+    ws = np.array([1,1,0,0])
+    wd = np.array([2,3,4,5])
+
+    flags = check_windsp_withdir(ws,wd)
+    '''
+    ws = np.array([1,0,0,2,3])
+    wd = np.array([2,3,3,5,5])
+
+    flags = check_winddir_withsp(ws,wd)
+
+    print(flags)
--- tests/unit/test_wind.py
+++ tests/unit/test_wind.py
@@ -16,44 +16,52 @@ class test_wind(unittest.TestCase):
         This dataset is somewhat arbitrary but meant to 
         be useful in a variety of ways.
         '''
-        data = [
-            (datetime.datetime(2020,8,7,0,0),0,0),
-            (datetime.datetime(2020,8,7,0,30),2,1),
-            (datetime.datetime(2020,8,7,0,45),2,1),
-            (datetime.datetime(2020,8,7,1,15),2,0),
-            (datetime.datetime(2020,8,7,1,30),0,0),
-            (datetime.datetime(2020,8,7,1,45),2,-2),
-            (datetime.datetime(2020,8,7,2,0),2,0),
-            (datetime.datetime(2020,8,7,2,15),3,-2),
-            (datetime.datetime(2020,8,7,2,30),2,-2),
-            (datetime.datetime(2020,8,7,2,45),5,-2),
-            (datetime.datetime(2020,8,7,3,15),0,0),
-            (datetime.datetime(2020,8,7,3,30),0,0)
-            ]
+        dts = [
+            datetime.datetime(2021,3,18,0,0),
+            datetime.datetime(2021,3,18,0,10),
+            datetime.datetime(2021,3,18,0,20),
+            datetime.datetime(2021,3,18,0,30),
+            datetime.datetime(2021,3,18,0,40),
+            datetime.datetime(2021,3,18,0,50),
+            datetime.datetime(2021,3,18,1,0),
+            datetime.datetime(2021,3,18,1,10)
+        ]
 
-        #Create dataframe
-        dtstamp = []
-        spvals = []
-        dirvals = []
-        for val in data:
-            dtstamp.append(val[0])
-            spvals.append(val[1])
-            dirvals.append(val[2])
+        #Test wind speeds and directions
+        #Consists of: 
+        # One section of zero winds associated with changing
+        # One section of zero winds with unchanging direction
+        # One section of non-zero winds with flatlined direction
+        spvals = [1,0,0,1,2,3,0,0]
+        dirvals = [30,55,65,60,60,65,22,22]
 
-        self.data = pd.DataFrame({'spvals':spvals,'dirvals':dirvals},index=dtstamp)
+        self.data = pd.DataFrame({'spvals':spvals,'dirvals':dirvals},index=dts)
 
     def tearDown(self):
         pass
 
-    def test_windspeed(self):
-        testratio = wind.check_windspeed(self.data[['spvals']])
-        trueratio = 2.745
+    def test_windsp_ratio(self):
+        testratio = wind.check_windsp_ratio(self.data)
+        trueratio = 0.31
 
         self.assertAlmostEqual(testratio,trueratio,3)
 
-    def test_winddir(self):
-        trueflags = [0,1,1,1,1,0,0,1,1,1,0,0]
-        testflags = wind.check_winddir(self.data)
+    def test_windsp_withdir(self):
+        trueflags = [0,1,1,0,0,0,0,0]
+        testflags = wind.check_windsp_withdir(
+            self.data['spvals'].to_numpy(),
+            self.data['dirvals'].to_numpy()
+            )
+
+        self.assertEqual(testflags,trueflags)
+    
+    def test_winddir_withsp(self):
+        trueflags = [0,0,0,1,1,0,0,0]
+        testflags = wind.check_winddir_withsp(
+            self.data['spvals'].to_numpy(),
+            self.data['dirvals'].to_numpy()
+            )
+
 
         self.assertEqual(testflags,trueflags)
 
